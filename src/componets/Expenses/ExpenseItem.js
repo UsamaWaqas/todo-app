6d2599d8b478faa19e281a57d0c5a83e96ed7768
UsamaWaqas <usamawaqas.m@gmail.com>
2023-09-07 00:09:45 +0500

    updated

---
+++ src/componets/Expenses/ExpenseItem.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
+
+const ExpenseItem = (props) => {
+//  function clickHandler() {
+
+//  }
+  const [title, setTitle] = useState(props.title);
+  // console.log('ExpenseItem evaluated by React');
+  
+  const clickHandler = () => {
+    setTitle('Updated!');
+    // console.log(title);
+  };
+
+  return (
+    <ul>
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
+      <button onClick={clickHandler}>Change Title</button>
+    </Card>
+    </ul>
+  );
+}
+
+export default ExpenseItem;
---
+++ src/componets/NewExpense/NewExpense.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import ExpenseForm from './ExpenseForm';
+import './NewExpense.css';
+
+const NewExpense = (props) => {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+    };
+    props.onAddExpense(expenseData);
+  };
+
+  return (
+    <div className='new-expense'>
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+    </div>
+  );
+};
+
+export default NewExpense;
